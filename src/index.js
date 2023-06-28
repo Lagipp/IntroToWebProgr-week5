17ee60c3024d3d566bca74ac3081972cb059724d
Miika Pynttäri <77153522+Lagipp@users.noreply.github.com>
2023-06-28 20:04:28 +0300

    exercise 4 complete

--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ const fetchData = async () => {
   // console.log(data.features.properties.name);
   // const features = data.features;
 
+  // const res_2 = await fetch(
+  //   "https://statfin.stat.fi/PxWeb/sq/4bb2c735-1dc3-4c5e-bde7-2165df85e65f"
+  // );
+  // const posMigration = await res_2.json();
+
+  // const res_3 = await fetch(
+  //   "https://statfin.stat.fi/PxWeb/sq/944493ca-ea4d-4fd9-a75c-4975192f7b6e"
+  // );
+  // const negMigration = await res_3.json();
+
   initMap(data);
 };
 
@@ -16,19 +26,6 @@ const initMap = (data) => {
   let map = L.map("map", {
     minZoom: -3
   });
-
-  // const municipalitiesData = [];
-  // let id = 0;
-
-  // data.features.forEach((el) => {
-  //   municipalitiesData.push({
-  //     id: id,
-  //     name: el.properties.nimi
-  //   });
-  //   id += 1;
-  // });
-
-  // console.log(municipalitiesData);
 
   const getFeature = (feature, layer) => {
     if (feature && feature.properties)
@@ -38,6 +35,10 @@ const initMap = (data) => {
   let geoJson = L.geoJSON(data, {
     onEachFeature: getFeature,
     weight: 2
+  }).addTo(map);
+
+  let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap"
   }).addTo(map);
 
   map.fitBounds(geoJson.getBounds());
