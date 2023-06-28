7470708b34cb917b34b3fcefdba44ac77eaf9091
Miika Pynttäri <77153522+Lagipp@users.noreply.github.com>
2023-06-28 19:00:18 +0300

    exercise 1 complete?

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css";
+
+const fetchData = async () => {
+  const url =
+    "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
+  const res = await fetch(url);
+  const data = await res.json();
+
+  initMap(data);
+};
+
+const initMap = (data) => {
+  let map = L.map("map", {
+    minZoom: -3
+  });
+
+  let geoJson = L.geoJSON(data, {
+    weight: 2
+  }).addTo(map);
+
+  map.fitBounds(geoJson.getBounds());
+};
+
+fetchData();
